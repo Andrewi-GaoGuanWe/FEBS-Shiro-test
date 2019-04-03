cd1dfc6dbd869d27a57ab6f933bdd3185863f17f
mrbird <852252810@qq.com>
2019-04-03 14:46:27 +0800

    update

--- src/main/java/cc/mrbird/common/aspect/LogAspect.java
+++ src/main/java/cc/mrbird/common/aspect/LogAspect.java
@@ -6,7 +6,6 @@ import cc.mrbird.common.util.IPUtils;
 import cc.mrbird.system.domain.SysLog;
 import cc.mrbird.system.domain.User;
 import cc.mrbird.system.service.LogService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.apache.shiro.SecurityUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -44,15 +43,11 @@ public class LogAspect {
     }
 
     @Around("pointcut()")
-    public Object around(ProceedingJoinPoint point) throws JsonProcessingException {
+    public Object around(ProceedingJoinPoint point) throws Throwable {
         Object result = null;
         long beginTime = System.currentTimeMillis();
-        try {
-            // 执行方法
-            result = point.proceed();
-        } catch (Throwable e) {
-            log.error(e.getMessage());
-        }
+        // 执行方法
+        result = point.proceed();
         // 获取request
         HttpServletRequest request = HttpContextUtils.getHttpServletRequest();
         // 设置IP地址
