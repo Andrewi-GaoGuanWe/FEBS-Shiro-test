4838c8d949d1013f0af98fb34286e675e1409d29
mrbird <852252810@qq.com>
2019-05-09 19:17:53 +0800

    添加runner

--- src/main/java/cc/mrbird/Application.java
+++ src/main/java/cc/mrbird/Application.java
@@ -1,18 +1,13 @@
 package cc.mrbird;
 
 import cc.mrbird.common.config.FebsProperties;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tk.mybatis.spring.annotation.MapperScan;
-
-import java.time.LocalDate;
-import java.time.LocalTime;
 
 @SpringBootApplication
 @EnableTransactionManagement
@@ -22,10 +17,8 @@ import java.time.LocalTime;
 @EnableAsync
 public class Application {
 
-    private static Logger log = LoggerFactory.getLogger(Application.class);
-
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-        log.info("《《《《《《 FEBS started up successfully at {} {} 》》》》》》", LocalDate.now(), LocalTime.now());
+        new SpringApplicationBuilder(Application.class)
+                .run(args);
     }
 }
