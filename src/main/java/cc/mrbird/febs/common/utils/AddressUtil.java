a8e9dd1691fcf76ddfc33097f97987c9bbcd7ace
mrbird <852252810@qq.com>
2019-06-17 20:46:21 +0800

    修复流未关闭

--- src/main/java/cc/mrbird/febs/common/utils/AddressUtil.java
+++ src/main/java/cc/mrbird/febs/common/utils/AddressUtil.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.Objects;
 
@@ -20,6 +21,7 @@ public class AddressUtil {
     private static Logger log = LoggerFactory.getLogger(AddressUtil.class);
 
     public static String getCityInfo(String ip) {
+        DbSearcher searcher = null;
         try {
             String dbPath = AddressUtil.class.getResource("/ip2region/ip2region.db").getPath();
             File file = new File(dbPath);
@@ -29,19 +31,24 @@ public class AddressUtil {
                 file = new File(dbPath);
                 FileUtils.copyInputStreamToFile(Objects.requireNonNull(AddressUtil.class.getClassLoader().getResourceAsStream("classpath:ip2region/ip2region.db")), file);
             }
-            int algorithm = DbSearcher.BTREE_ALGORITHM;
             DbConfig config = new DbConfig();
-            DbSearcher searcher = new DbSearcher(config, file.getPath());
-            Method method = null;
-            method = searcher.getClass().getMethod("btreeSearch", String.class);
-            DataBlock dataBlock = null;
+            searcher = new DbSearcher(config, file.getPath());
+            Method method = searcher.getClass().getMethod("btreeSearch", String.class);
             if (!Util.isIpAddress(ip)) {
                 log.error("Error: Invalid ip address");
             }
-            dataBlock = (DataBlock) method.invoke(searcher, ip);
+            DataBlock dataBlock = (DataBlock) method.invoke(searcher, ip);
             return dataBlock.getRegion();
         } catch (Exception e) {
             log.error("获取地址信息异常", e);
+        }finally {
+            if (searcher !=null) {
+                try {
+                    searcher.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
         return "";
     }
